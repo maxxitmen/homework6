6f5bcc99ea8a7afe94d4ad6a230f4009a7b736b7
maxxitmen <61408791+maxxitmen@users.noreply.github.com>
2024-04-24 19:33:29 +0300

    Add files via upload

---
+++ Homework6.py
@@ -0,0 +1,13 @@
+my_list = ['apple', 'banana', 'melon', 'strawberry', 'peach', 'cucumber', 'onion']
+print(my_list)
+print(my_list[0])
+print(my_list[-1])
+print(my_list[2:5])
+my_list[2] = 'watermelon'
+print(my_list[2])
+
+my_dict = {'apple': 'яблоко', 'banana': 'банан', 'melon': 'арбуз', 'strawberry': 'клубника', 'peach': 'персик', 'cucumber': 'огурец', 'onion': 'лук'}
+print(my_dict)
+my_dict['melon'] = 'дыня'
+my_dict['orange'] = 'апельсин'
+print(my_dict)
